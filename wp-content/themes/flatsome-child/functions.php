dba9f0b89bca4588c982cec9c8300941fb89e0a2
haunttvo <haunt@tinhvan.com>
2019-02-21 17:49:15 +0700

    update naman

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,8 @@
 <?php
 // Add custom Theme Functions here
+
+add_action('wp_enqueue_scripts', 'my_script_hautvo');
+
+function my_script_hautvo(){
+    wp_enqueue_style( 'custom_stylesheet', get_stylesheet_directory_uri() . '/asset/css/style.css' );
+}
