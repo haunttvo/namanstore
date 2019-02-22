30edccbf11008a7e70dec0a6a2c566e0af65dbbb
haunttvo <haunt@tinhvan.com>
2019-02-22 17:50:04 +0700

    naman

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -6,3 +6,9 @@ add_action('wp_enqueue_scripts', 'my_script_hautvo');
 function my_script_hautvo(){
     wp_enqueue_style( 'custom_stylesheet', get_stylesheet_directory_uri() . '/asset/css/style.css' );
 }
+
+add_filter('woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text');
+
+function woo_custom_cart_button_text() {
+return __('Thêm vào giỏ', 'woocommerce');
+}
