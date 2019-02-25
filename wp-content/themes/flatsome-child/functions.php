8ac294a9b93c4a1ee23ca9cb7c9c16e41f26ae01
haunttvo <haunt@tinhvan.com>
2019-02-25 07:26:02 +0700

    code 25/2

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -12,3 +12,30 @@ add_filter('woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_butto
 function woo_custom_cart_button_text() {
 return __('Thêm vào giỏ', 'woocommerce');
 }
+
+
+function fn_info_us(){
+    ob_start();
+    ?>
+    <div class="info_us">
+        <ul class="ul_info_us">
+            <li>
+                <i class="icon-map-pin-fill"></i>
+                <p>193 Núi thành, Q.Hải Châu, TP.Đà Nẵng</p>
+            </li>
+            <li>
+            <i class="icon-phone"></i>
+                <p>0973.66.99.81</p>
+            </li>
+            <li>
+                <i class="icon-envelop"></i>
+                <p>namanstore@gmail.com</p>
+            </li>
+        </ul>
+    </div>
+    <?php
+    $context = ob_get_contents();
+    ob_end_clean();
+    return $context;
+}
+add_shortcode('info_us', 'fn_info_us');
