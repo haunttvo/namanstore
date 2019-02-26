9785940c8a953d7115b2fcd5d7210d924742ebf5
haunttvo <haunt@tinhvan.com>
2019-02-26 16:59:55 +0700

    naman 26/2

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -70,3 +70,29 @@ add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' )
 function woo_custom_breadrumb_home_url() {
     return 'http://woocommerce.com';
 }
+
+/** acf load field huong dan chon size */
+function acf_load_field_tut_choice_size( $field ) {
+    $arr_tut_size = new wp_query(array(
+        'post_type' => 'tut_choice_size',
+        'post_status' => 'publish'
+    ));    
+    $arr_tut_size = $arr_tut_size->posts;
+    $res_field = [];
+    array_unshift($res_field, '--Hướng dẫn chọn size--');
+    foreach ($arr_tut_size as $key => $value) {
+        $res_field = $res_field + array( $value->ID => $value->post_title );
+    }
+    $field['choices'] = $res_field;
+    return $field;
+}
+add_filter('acf/load_field/name=tut_choice_size', 'acf_load_field_tut_choice_size');
+
+function fn_woo_choice_size() {  
+    $post_id = get_the_ID();
+    $id_choice_size = get_field('tut_choice_size', $post_id);
+    $the_content = get_post_field( 'post_content', $id_choice_size);
+    if( !empty($id_choice_size) || $id_choice_size != 0 )
+    echo '<a class="link_choice_size" href="#popup_choice_size"><i>Hướng dẫn chọn size</i></a>'.do_shortcode('[lightbox id="popup_choice_size" width="1200px" padding="20px"]'. apply_filters('the_content',$the_content) .'[/lightbox]');
+}    
+add_action( 'woocommerce_single_product_summary', 'fn_woo_choice_size', 25 ); 
